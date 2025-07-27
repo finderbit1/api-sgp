58b6b66bdc4c86848b6eddd3e0f4c81d153f48c2
mrmateussiilva <mateuspython3010@gmail.com>
2025-07-26 23:45:19 -0300

    add: restante do projeto

---
+++ database/db_fake.py
@@ -0,0 +1,52 @@
+from datetime import date
+from models.pedido import Cliente,Pedido,List
+
+
+# Simulando um banco de dados em memória
+pedidos_fake: List[Pedido] = [
+    Pedido(
+        numero="001",
+        cliente="Mateus José da Silva",
+        data=str(date.today().strftime("%d/%m/%Y")),
+        status="pendente",
+        prioridade=True,
+        financeiro=False,
+        sublimação=False,
+        costura=False,
+        expedicao=False
+    ),
+    Pedido(
+        numero="002",
+        cliente="Carlos Souza",
+        data=str(date.today().strftime("%d/%m/%Y")),
+        status="pronto",
+        prioridade=False,
+        financeiro=True,
+        sublimação=True,
+        costura=True,
+        expedicao=True
+    )
+]
+
+clientes: List[Cliente]= [
+    Cliente(
+        id=0,
+        nome="Mateus",
+        cep="29701340",
+        cidade="colatina",
+        estado="es",
+        telefone="27 995900071"
+    ),
+        Cliente(
+        id=1,
+        nome="Breno Polezi",
+        cep="29701340",
+        cidade="linhares",
+        estado="es",
+        telefone="27 000900071"
+    ),
+]
+
+
+
+
---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from models.pedido import *
+from database.db_fake import *
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Em produção, use o IP/URL específico
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/pedidos", response_model=List[Pedido])
+def listar_pedidos():
+    return pedidos_fake
+
+@app.post("/pedidos")
+def criar_pedido(ficha:Ficha):
+    ficha_dict = ficha.dict()
+    print(ficha_dict)
+
+    return ficha_dict
+
+
+@app.get("/clientes",response_model=List[Cliente])
+def listar_clientes():
+    return clientes
