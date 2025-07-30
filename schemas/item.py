10a104600564d70e7aa19aeea3b53609b74b0b09
mrmateussiilva <mateuspython3010@gmail.com>
2025-07-30 17:01:37 -0300

    refactor: Removendo os arquivos obsoletos de schemas

--- schemas/item.py
+++
@@ -1,9 +0,0 @@
-from pydantic  import BaseModel
-
-
-class Item(BaseModel):
-    nome: str
-    quantidade: int
-    largura: str  # ou float se você estiver convertendo "1,58" → 1.58
-    altura: str   # idem
-    # ... outros campos
--- schemas/payments.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class Payments(BaseModel):
-    id: int 
-    name: str
-    
